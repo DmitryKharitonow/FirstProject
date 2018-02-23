64a2e8744dade8190191f7f69d2fc8f56220c948
DmitryKharitonow <36765073+DmitryKharitonow@users.noreply.github.com>
2018-02-23 21:24:25 +0700

    script

---
+++ script.js
@@ -0,0 +1,18 @@
+jQuery('document').ready(function(){
+	
+	jQuery('button').on('click',function(){
+		
+		var value1,value2,va;
+		
+		value1 = jQuery("#val1").val();
+		value2 = jQuery("#val2").val();
+		
+		value1 = parseInt(value1);
+		value2 = parseInt(value2);
+		
+		alert(value1 + value2);
+	
+	});
+	
+	 
+});
